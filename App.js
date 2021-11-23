64df46fc946812fc54b41f24731b23e7ec634ee6
jonmersha@gmail.com <jonmersha@gmail.com>
2021-11-23 07:33:09 -0500

    multiple screen

--- App.js
+++ App.js
@@ -1,42 +1,188 @@
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 
-export default function App() {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  const getMovies = async () => {
-     try {
-      const response = await fetch('https://store.besheger.com/retailer');
-      const json = await response.json();
-      setData(json.details);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
+function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data}
-          keyExtractor={({ Retailer_id }, index) => Retailer_id}
-          renderItem={({ item }) => (
-
-            <Text>{item.first_name}, {item.midle_name},{item.last_name},{item.last_name}</Text>
- 
-          )}
-        />
-      )}
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
-};
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
+
+// import * as React from 'react';
+// import { Text, View, StyleSheet } from 'react-native';
+// import Constants from 'expo-constants';
+
+// // You can import from local files
+// import AssetExample from './asset';
+
+// // or any pure javascript modules available in npm
+// import { Card } from 'react-native-paper';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.paragraph}>
+//         Change code in the editor and watch it change on your phone! Save to get a shareable url.
+//       </Text>
+//       <Card>
+//         <AssetExample />
+//       </Card>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     paddingTop: Constants.statusBarHeight,
+//     backgroundColor: '#ecf0f1',
+//     padding: 8,
+//   },
+//   paragraph: {
+//     margin: 24,
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//     textAlign: 'center',
+//   },
+// });
+
+
+
+// import * as React from 'react';
+// import {Button, View, Text } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// function Details() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Detail Screen</Text>
+//       <Button
+//         title="Go to Details"
+//         onPress={() => navigation.navigate('Home')}
+//       />
+//     </View>
+//   );
+// }
+
+// function Home({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen</Text>
+//       <Button
+//         title="Go to Details"
+//         onPress={() => navigation.navigate('Details')}
+//       />
+//     </View>
+//   );
+// }
+
+// const Stack = createNativeStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Home">
+//         <Stack.Screen name="Home" component={Home} />
+        
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+// export default App;
+// import * as React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+//   );
+// }
+
+// import React, { useEffect, useState } from 'react';
+// import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+
+
+
+// export default function App() {
+//   const [isLoading, setLoading] = useState(true);
+//   const [data, setData] = useState([]);
+
+//   const getMovies = async () => {
+//      try {
+//       const response = await fetch('https://store.besheger.com/retailer');
+//       const json = await response.json();
+//       setData(json.details);
+//     } catch (error) {
+//       console.error(error);
+//     } finally {
+//       setLoading(false);
+//     }
+//   }
+
+
+//   useEffect(() => {
+//     getMovies();
+//   }, []);
+
+//   return (
+//     <View style={{ flex: 1, padding: 24 }}>
+//       {isLoading ? <ActivityIndicator/> : (
+//         <FlatList
+//           data={data}
+//           keyExtractor={({ Retailer_id }, index) => Retailer_id}
+//           renderItem={({ item }) => (
+
+//             <Text>{item.first_name}, {item.midle_name},{item.last_name},{item.last_name}</Text>
+ 
+//           )}
+//         />
+//       )}
+//     </View>
+//   );
+// };
